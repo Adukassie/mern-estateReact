c21f3fd84ab4b38cd115dcebc024223ec7f880ac
Adukasse <adugnadagnaw01@gmail.com>
2023-10-03 11:18:06 -0500

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
 app.listen(3000, () => {
   console.log("server listening on port 3000!!");
 });
 app.get("/", function(req, res) {
-    res.end('Welcome')
+  res.end("Welcome");
 });
