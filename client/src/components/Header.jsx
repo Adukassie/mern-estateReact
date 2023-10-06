a25149e013fa881b14f34fa76686f9d1f4332697
Adukasse <adugnadagnaw01@gmail.com>
2023-10-05 22:17:22 -0500

    update header and make profile page private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,20 @@
-import React from "react";
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+
 function Header() {
+  const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+  
+   const handleSubmit = (e) => {
+     e.preventDefault();
+     const urlParams = new URLSearchParams(window.location.search);
+     urlParams.set("searchTerm", searchTerm);
+     const searchQuery = urlParams.toString();
+     navigate(`/search?${searchQuery}`);
+   };
   return (
     <header className="bg-slate-200 shadow-md">
       <div
@@ -15,7 +28,10 @@ function Header() {
           </h1>
         </Link>
 
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-slate-100 p-3 rounded-lg flex items-center"
+        >
           <input
             type="text"
             placeholder="Search"
@@ -34,15 +50,21 @@ function Header() {
           </Link>
 
           <Link to="/about">
-       
             <li className="hidden sm:inline text-slate-700 hover:underline">
               About
             </li>
           </Link>
 
-          <Link to="/sign-in">
-            {" "}
-            <li className=" text-slate-700 hover:underline">Sign in</li>
+          <Link to="/profile">
+            {currentUser ? (
+              <img
+                className="rounded-full h-7 w-7 object-cover"
+                src={currentUser.avatar}
+                alt="profile"
+              />
+            ) : (
+              <li className=" text-slate-700 hover:underline"> Sign in</li>
+            )}
           </Link>
         </ul>
       </div>
